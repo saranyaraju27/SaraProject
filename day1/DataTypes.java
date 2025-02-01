4329467a4d77c585438fadb8f739381b0a89681c
saranyaraju27 <42091770+saranyaraju27@users.noreply.github.com>
2025-02-01 17:05:25 +0530

    Add files via upload
    day1

---
+++ day1/DataTypes.java
@@ -0,0 +1,65 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// datatype varaiableName = value //
+		//Integer 
+		//byte
+		byte ageOf=5;
+		System.out.println(ageOf);
+
+		
+		//short
+		short ageOfe=35;
+		System.out.println(ageOfe);
+
+		
+		//int  
+		int saranyaDaku=3000;
+		System.out.println(saranyaDaku);
+
+		
+		//long -> use this letter with L orl
+		
+		long mobileNumber=976787567l;
+		
+		System.out.println(mobileNumber);
+
+		//float->onluable to 6,7 decimal
+		
+		float rateOfInterest=7.35F;
+		System.out.println(rateOfInterest);
+
+		
+		//double -> use more accurate value
+		//it will accept more than float 
+		
+		double rate2=2.3457645;
+		float rate3=2.45466f;
+	System.out.println(rate3);
+	System.out.println(rate2);
+	
+	// Char -> it can store only one charcter
+	//it may alphates, num, special character
+	//always it will come with single quotes
+	
+	char intial='m';
+	System.out.println(intial);
+	char special='$';
+			System.out.println(special);
+	//boolean
+			//true or false statement
+			
+			boolean doYouKnow=true;
+//String - collections of charcters
+			String name="daksha";
+			System.out.println(name);
+
+			
+			
+		
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+// Blueprint to create object or to write the structure//
+
+//AM(Access Modifier)  Keyword  ClassName
+
+// to see the output we need to use sysout//
+public class HelloTestLeaf {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("myfirstcode");
+		//If numbers means no need to give double code
+		System.out.println(123);
+
+		
+		
+	}
+
+}
